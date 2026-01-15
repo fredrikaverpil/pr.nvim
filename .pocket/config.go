996b7a7a9751e988705e1180264c198608474749
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-01-15 20:40:03 +0100

    feat: migrate from Taskfile to Pocket
    - Replace Taskfile with Pocket for build automation
    - Consolidate tool management into .pocket directory
    - Use embedded workflows from Pocket
    
    - Remove Taskfile.yml and Taskfile.lua.yml
    - Remove tools directory (stylua version now managed by Pocket)
    - Add .pocket configuration with Lua workflow
    - Add GitHub workflow templates (pocket, pr, release, stale, sync)
    - Remove managed and unmanaged workflows

---
+++ .pocket/config.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/fredrikaverpil/pocket"
+	"github.com/fredrikaverpil/pocket/tasks/github"
+	"github.com/fredrikaverpil/pocket/tasks/lua"
+)
+
+// Config is the pocket configuration for this project.
+var Config = pocket.Config{
+	AutoRun: pocket.Serial(
+		// Lua workflow (format)
+		pocket.Paths(lua.Workflow()).DetectBy(lua.Detect()),
+	),
+	ManualRun: []pocket.Runnable{
+		github.Workflows,
+	},
+	Shim: &pocket.ShimConfig{
+		Posix: true,
+	},
+}
---
+++ .pocket/go.mod
@@ -0,0 +1,13 @@
+module pok
+
+go 1.25.5
+
+require github.com/fredrikaverpil/pocket v0.0.0-20260115192854-11c4812b0962
+
+require (
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/term v0.39.0 // indirect
+)
+
+replace github.com/fredrikaverpil/pocket => ../../pocket
